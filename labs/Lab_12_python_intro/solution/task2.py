581cee499896e08656be308aad610b150d9ac18c
LastDayWithLess <2003kirillgusko@gmail.com>
2023-12-25 00:09:36 +0300

    Исправил 1 и 2 задание

--- labs/Lab_12_python_intro/solution/task2.py
+++ labs/Lab_12_python_intro/solution/task2.py
@@ -1,19 +1,34 @@
+import heapq
+
+def calculate_medians(nums):
+    min_heap = []  # Мин-куча для хранения большей половины элементов
+    max_heap = []  # Макс-куча для хранения меньшей половины элементов
+    medians_sum = 0
+    
+    for i, num in enumerate(nums):
+        if not max_heap or num < -max_heap[0]:
+            heapq.heappush(max_heap, -num)
+        else:
+            heapq.heappush(min_heap, num)
+        
+        # Балансируем кучи
+        if len(max_heap) > len(min_heap) + 1:
+            heapq.heappush(min_heap, -heapq.heappop(max_heap))
+        elif len(min_heap) > len(max_heap):
+            heapq.heappush(max_heap, -heapq.heappop(min_heap))
+        
+        # Находим текущую медиану
+        if i % 2 == 0:
+            medians_sum += -max_heap[0] if len(max_heap) > len(min_heap) else min_heap[0]
+        else:
+            medians_sum += -max_heap[0]
+    
+    return medians_sum
+
+# Считываем входные данные
 N = int(input())
 numbers = list(map(int, input().split()))
 
-medians_sum = 0
-sorted_numbers = []
-
-for i in range(N):
-    sorted_numbers.append(numbers[i])
-    sorted_numbers.sort()
-    median_index = (i + 1) // 2
-    
-    if (i + 1) % 2 == 0:
-        median = sorted_numbers[median_index - 1]
-    else:
-        median = sorted_numbers[median_index]
-    
-    medians_sum += median
-
-print(int(medians_sum))
+# Вычисляем и выводим сумму медианных значений
+result = calculate_medians(numbers)
+print(result)
